3b3767c4e9993502dd874422f78ecef70c94a363
Paulo Guilherme <pgsilva0698@gmail.com>
2022-12-20 00:06:28 -0300

    add haversine functions 👩‍🏫

---
+++ how-many-movies-in-trip/src/main/kotlin/domain/CatsBasket.kt
@@ -0,0 +1,12 @@
+@file:JvmName("CatsBasket")
+
+package domain
+
+const val EARTH_RADIUS_KM: Double = 6372.8
+const val PLANE_AVERAGE_SPEED_KM_H: Double = 800.0
+const val CAR_AVERAGE_SPEED_KM_H: Double = 80.0
+
+typealias Lat = Double
+typealias Lon = Double
+typealias Coordinate = Pair<Lat, Lon>
+typealias Hours = Double
---
+++ how-many-movies-in-trip/src/main/kotlin/domain/Haversine.kt
@@ -0,0 +1,23 @@
+package domain
+
+import java.math.BigDecimal
+import java.math.RoundingMode
+import kotlin.math.*
+
+infix fun Coordinate.distanceKmTo(to: Coordinate): Double {
+    val distanceLat = Math.toRadians(to.first - this.first)
+    val distanceLon = Math.toRadians(to.second - this.second)
+
+    val originLat = Math.toRadians(this.first)
+    val destinationLat = Math.toRadians(to.first)
+
+    val a = sin(distanceLat / 2).pow(2.0) + sin(distanceLon / 2).pow(2.0) * cos(originLat) * cos(destinationLat)
+    val c = 2 * asin(sqrt(a))
+
+    return BigDecimal(EARTH_RADIUS_KM * c).setScale(2, RoundingMode.HALF_UP).toDouble()
+}
+
+fun timeSpentAt(distanceKm: Double, speedKmH: Double): Hours {
+    val time = distanceKm / speedKmH
+    return BigDecimal(time).setScale(2, RoundingMode.HALF_UP).toDouble()
+}
